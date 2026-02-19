3370a3771c615f750f29bd94dd5f7d6b51689747
Nit <nitithon.taksaphan@gmail.com>
2026-02-19 15:40:32 +0700

    feat: Enhance payment flow with countdown timer and QR code status updates; improve error responses in checkout API.

---
+++ src/pages/payment/index.js
@@ -0,0 +1,10 @@
+import PaymentForm from "../../features/payment/PaymentForm";
+
+
+export default function PaymentPage() {
+  return (
+    <div>
+      This is the payment page
+    </div>
+  );
+}
