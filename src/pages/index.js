75c1342fc6fd6d6fe0a0e57d71f78dbb454cce9b
thanakorn <thanakorn.stsb@gmail.com>
2026-02-23 01:54:54 +0700

    refactor: replace Home component content with Homepage component for improved structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,12 @@ import NavBar from "@/common/navbar/NavBar";
 import SubFooter from "@/common/SubFooter";
 import Footer from "@/common/Footer";
 import Link from "next/link"
+import Homepage from "./homepage";
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
-      <div className="min-h-screen bg-gray-100 p-4 flex items-center justify-center">
-      <Link  href="/common-test" className="bg-blue-500 text-white px-4 py-2 rounded-md" >Go to Common Test</Link>
-      </div>        
-      <SubFooter />
-      <Footer />
+      <Homepage />
     </>
   );
 }
