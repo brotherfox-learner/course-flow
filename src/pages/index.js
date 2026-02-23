6f21eea19e195a0f266bc2b01edf0bab05414cb7
thanakorn <thanakorn.stsb@gmail.com>
2026-02-23 15:00:01 +0700

    refactor: remove unused imports from NavBar and index.js to clean up codebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,3 @@
-import NavBar from "@/common/navbar/NavBar";
-import SubFooter from "@/common/SubFooter";
-import Footer from "@/common/Footer";
-import Link from "next/link"
 import Homepage from "./homepage";
 
 export default function Home() {
